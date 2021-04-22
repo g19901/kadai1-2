03c1b4f26de491124cced20f5e7e56f74cd481ec
g19901 <75403429+g19901@users.noreply.github.com>
2021-04-22 15:48:28 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
- public static void main(String args){
+ public static void main(String[] args){
    System.out.println("好きな食べ物はオムライスです。");
    System.out.println("その理由は卵がふわふわなのでとても美味しいからです。");
  }
